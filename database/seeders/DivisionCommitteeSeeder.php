798fc02e9d56d438e059d6e22c2a5c39febcd08e
boniyeamincse <boniyeamin.cse1@gmail.com>
2026-04-18 22:25:12 +0600

    Make division committee seeder idempotent

--- database/seeders/DivisionCommitteeSeeder.php
+++ database/seeders/DivisionCommitteeSeeder.php
@@ -10,6 +10,19 @@ use Illuminate\Support\Str;
 
 class DivisionCommitteeSeeder extends Seeder
 {
+    private function upsertCommitteeBySlug(string $slug, array $attributes): Committee
+    {
+        $committee = Committee::withTrashed()->firstOrNew(['slug' => $slug]);
+        $committee->fill($attributes);
+        $committee->save();
+
+        if ($committee->trashed()) {
+            $committee->restore();
+        }
+
+        return $committee;
+    }
+
     public function run(): void
     {
         $centralType = CommitteeType::query()->where('slug', 'central')->first();
@@ -20,17 +33,14 @@ class DivisionCommitteeSeeder extends Seeder
             return;
         }
 
-        $central = Committee::updateOrCreate(
-            ['slug' => 'central-committee'],
-            [
-                'committee_no' => 'COM-CEN-000',
-                'name' => 'Central Committee',
-                'committee_type_id' => $centralType->id,
-                'code' => 'CEN',
-                'status' => 'active',
-                'is_current' => true,
-            ]
-        );
+        $central = $this->upsertCommitteeBySlug('central-committee', [
+            'committee_no' => 'COM-CEN-000',
+            'name' => 'Central Committee',
+            'committee_type_id' => $centralType->id,
+            'code' => 'CEN',
+            'status' => 'active',
+            'is_current' => true,
+        ]);
 
         $divisions = Division::query()->orderBy('id')->get(['id', 'name_en']);
 
@@ -38,19 +48,16 @@ class DivisionCommitteeSeeder extends Seeder
             $slug = Str::slug($division->name_en . ' Division Committee');
             $code = strtoupper(substr(preg_replace('/[^A-Za-z]/', '', $division->name_en), 0, 3));
 
-            Committee::updateOrCreate(
-                ['slug' => $slug],
-                [
-                    'committee_no' => sprintf('COM-DIV-%03d', (int) $division->id),
-                    'name' => sprintf('%s Division Committee', $division->name_en),
-                    'committee_type_id' => $divisionType->id,
-                    'parent_id' => $central->id,
-                    'code' => $code ?: 'DIV',
-                    'division_id' => $division->id,
-                    'status' => 'active',
-                    'is_current' => true,
-                ]
-            );
+            $this->upsertCommitteeBySlug($slug, [
+                'committee_no' => sprintf('COM-DIV-%03d', (int) $division->id),
+                'name' => sprintf('%s Division Committee', $division->name_en),
+                'committee_type_id' => $divisionType->id,
+                'parent_id' => $central->id,
+                'code' => $code ?: 'DIV',
+                'division_id' => $division->id,
+                'status' => 'active',
+                'is_current' => true,
+            ]);
         }
 
         $this->command->info('Division committees seeded for all divisions.');
